a94991e791bfab9bb0f8c2873b44ce3296ac9c4d
tuan3572011 <11130046@st.hcmuaf.edu.vn>
2015-09-26 23:11:32 +0700

    complete

--- src/LiveScore.java
+++ src/LiveScore.java
@@ -18,40 +18,50 @@ public class LiveScore {
 		URL oracle = new URL("http://www.livescore.com/soccer/live/");
 		URLConnection yc = oracle.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
 		StringBuffer pageHTML = new StringBuffer();
 		String inputLine;
 		while ((inputLine = in.readLine()) != null) {
 			pageHTML.append(inputLine);
 		}
 		in.close();
+
 		int startContentTagLength = "<div class=\"content\">".length();
 		int endContentTagLength = "</div>".length() + 1;
-
 		int indexOfContentTag = pageHTML.indexOf("<div class=\"content\">");
 		int indexOfContentTagEnd = pageHTML.indexOf("<div class=\"right-bar\">");
+
 		String htmlContent = pageHTML.substring(indexOfContentTag + startContentTagLength, indexOfContentTagEnd
 				- endContentTagLength);
 
-		String beginMatchTag = "<div class=\"ply tright name\">";
-		String endMatchTag = "<div class=\"star hidden\" data-type=\"star\">";
+		final String BEGIN_MATCH_TAG = "<div class=\"ply tright name\">";
+		final String END_MATCH_TAG = "<div class=\"star hidden\" data-type=\"star\">";
+
 		boolean isAnyMatch = true;
 		int indexStart = -1;
 		int indexEnd = -1;
 		List<String> matches = new ArrayList<String>();
-		String match = "";
 		while (isAnyMatch) {
+			String match = "";
 			isAnyMatch = false;
-			indexStart = htmlContent.indexOf(beginMatchTag);
-			indexEnd = htmlContent.indexOf(endMatchTag);
+			indexStart = htmlContent.indexOf(BEGIN_MATCH_TAG);
+			indexEnd = htmlContent.indexOf(END_MATCH_TAG);
 			if (indexStart > 0 && indexEnd > 0) {
 				isAnyMatch = true;
 				match = htmlContent.substring(indexStart, indexEnd);
 				matches.add(match);
-				htmlContent = htmlContent.substring(indexEnd + endMatchTag.length());
+				htmlContent = htmlContent.substring(indexEnd + END_MATCH_TAG.length());
 			}
+			System.out.println(match);
 		}
-		String march = matches.get(0);
-		
+
+		List<FootBall> footBalls = new ArrayList<FootBall>();
+		for (String match : matches) {
+			FootBall fb = new FootBall(match);
+			System.out.println(fb.getCompetitor1() + " " + fb.getCompetitor1Score() + "----" + fb.getCompetitor2()
+					+ " " + fb.getCompetitor2Score());
+			footBalls.add(fb);
+		}
 
 	}
 }
